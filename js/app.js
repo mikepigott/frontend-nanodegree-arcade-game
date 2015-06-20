49a8707351b079d9cc64079b89fd21075d633821
mikepigott <mpigott@ironponyexpress.org>
2015-06-20 11:28:27 -0400

    Adding bounds to the game and checks based on images if bug is touching.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-var Character = function(sprite, startX, startY, speed) {
+var Character = function(sprite, startX, startY, width, height) {
     this.sprite = sprite;
     this.x = startX;
     this.y = startY;
-    this.speed = speed;
+    this.width = width;
+    this.height = height;
 }
 
 // Draw the character on the screen
@@ -11,8 +12,9 @@ Character.prototype.render = function() {
 }
 
 // Enemies our player must avoid
-var Enemy = function(startX, startY, speed) {
-    Character.call(this, 'images/enemy-bug.png', startX, startY, speed);
+var Enemy = function(startX, startY, width, height, speed) {
+    Character.call(this, 'images/enemy-bug.png', startX, startY, width, height);
+    this.speed = speed;
 }
 
 Enemy.prototype = Object.create(Character.prototype);
@@ -28,13 +30,22 @@ Enemy.prototype.update = function(dt) {
     if (this.x > 500) {
         this.x = -100;
     }
+
+    var isInWidth = ((this.x <= (player.x + player.width)) && ((this.x + this.width) >= player.x));
+    var isInHeight = ((this.y <= (player.y + player.height)) && ((this.y + this.height ) >= player.y));
+
+    if (isInWidth && isInHeight) {
+
+        //console.log("Bug killed player!: Player: {x:" + player.x + " x + width: " + (player.x + player.width) + " y: " + player.y + " y + height: " + (player.y + player.height));
+        //console.log("Bug killed player!:    Bug: {x:" + this.x + " x + width: " + (this.x + this.width) + " y: " + this.y + " y + height: " + (this.y + this.height));
+    }
 }
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(startX, startY, speed) {
-  Character.call(this, 'images/char-boy.png', startX, startY, speed);
+var Player = function(startX, startY, width, height) {
+  Character.call(this, 'images/char-boy.png', startX, startY, width, height);
 }
 
 Player.prototype = Object.create(Character.prototype);
@@ -45,25 +56,34 @@ Player.prototype.handleInput = function(key) {
         return;
     }
     console.log("Received input: " + key);
-    if (key == 'down') {
-        this.y = this.y + 75;
-    } else if (key == 'up') {
-        this.y = this.y - 75;
-    } else if (key == 'left') {
-        this.x = this.x - 100;
-    } else if (key == 'right') {
-        this.x = this.x + 100;
+    if ((key == 'down') && (this.y <= 400)) {
+        this.y = this.y + this.height;
+    } else if ((key == 'up') && (this.y >= 0)) {
+        this.y = this.y - this.height;
+    } else if ((key == 'left') && (this.x >= 25)) {
+        this.x = this.x - this.width;
+    } else if ((key == 'right') && (this.x <= 350)) {
+        this.x = this.x + this.width;
     }
+
+    console.log("player.x = " + player.x + ", player.y = " + player.y);
 }
 
-Player.prototype.update = function(dt) {
+Player.prototype.update = function() {
+    for (var enemy in allEnemies) {
+        if (((enemy.x <= (this.x + this.width)) && ((enemy.x + enemy.width) >= this.x))
+            && ((enemy.y <= (this.y + this.height)) && ((enemy.y + enemy.height ) >= this.y))) {
+
+            //console.log("Player committed suicide!");
+        }
+    }
 }
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = new Array(new Enemy(0, 0, 50, 1));
-var player = new Player(100, 150, 0);
+var allEnemies = new Array(new Enemy(0, 0, 101, 171, 50));
+var player = new Player(75, 425, 25, 25);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
